a6caa57c4d074a8a0d2c45d4ce6e214fe47669c9
lee <sleechengn@live.cn>
2026-06-27 17:41:01 +0800

    initialize

---
+++ tmux.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+if [ ! "$(which tmux)" ]; then
+
+        if [ "$(which apt)" ]; then
+                apt install -y tmux fish
+        else
+                if [ "$(which apk)" ]; then
+                        apk add tmux fish
+                        else
+                        exit 1
+                fi
+        fi
+
+        if [ ! -e "~/.tmux.conf" ]; then
+cat > ~/.tmux.conf <<EOF
+set -g mouse on
+unbind -n MouseDown3Pane
+set -g default-command fish
+EOF
+tmux source ~/.tmux.conf
+        fi
+else
+        if [ ! -e "~/.tmux.conf" ]; then
+cat > ~/.tmux.conf <<EOF
+set -g mouse on
+unbind -n MouseDown3Pane
+set -g default-command fish
+EOF
+tmux source ~/.tmux.conf
+        fi
+fi
+
+if [ "$(tmux ls|grep '^default.*')" ]; then
+        tmux a -t default
+else
+        tmux new -s default
+fi
