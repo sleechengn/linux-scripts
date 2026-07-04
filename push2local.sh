c292c14f62d9dbdc48e127fcbebe4442a4b5e74c
lee <sleechengn@live.cn>
2026-07-04 11:39:56 +0800

    保存

--- push2local.sh
+++ push2local.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -e
 IMN=$1
+echo "image:$IMN"
 if [ "$(echo $IMN|grep -F 192.168.13.73:5000)" ]; then
 	docker push $IMN
 else
-    repo=$(echo $IMG|awk -F ':' '{print $1}')
-    tag=$(echo $IMG|awk -F ':' '{print $2}')
+    repo=$(echo $IMN|awk -F ':' '{print $1}')
+    tag=$(echo $IMN|awk -F ':' '{print $2}')
+	echo "repo: $repo"
+	echo "tag: $tag"
+	if [ ! "$tag" ]; then
+		tag="latest"
+	fi
 	docker tag $IMN 192.168.13.73:5000/$repo-$(arch):$tag
 	docker push 192.168.13.73:5000/$repo-$(arch):$tag
 fi
