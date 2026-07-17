d6cde833a440d27a8ede2e25f79ed272abb3665b
lee <sleechengn@live.cn>
2026-07-17 08:59:31 +0800

    保存

--- tmux.sh
+++ tmux.sh
@@ -15,6 +15,9 @@ if [ ! "$(which tmux)" ]; then
                         fi
                 fi
         fi
+fi
+
+if [ "$(which tmux)" ]; then
 
         if [ ! -e "~/.tmux.conf" ]; then
 cat > ~/.tmux.conf <<EOF
@@ -22,21 +25,20 @@ set -g mouse on
 unbind -n MouseDown3Pane
 set -g default-command fish
 EOF
-tmux source ~/.tmux.conf
+        tmux source ~/.tmux.conf
         fi
-else
-        if [ ! -e "~/.tmux.conf" ]; then
-cat > ~/.tmux.conf <<EOF
-set -g mouse on
-unbind -n MouseDown3Pane
-set -g default-command fish
-EOF
-tmux source ~/.tmux.conf
+
+        if [ "$(tmux ls|grep '^default.*')" ]; then
+                tmux a -t default
+        else
+                tmux new -s default
         fi
+
+        if [ $(id -u $(whoami)) -eq 0 ] && [ ! -e "/usr/bin/t" ] ; then
+                cp $0 /usr/bin/t
+                chmod 755 /usr/bin/t
+        fi
+
 fi
 
-if [ "$(tmux ls|grep '^default.*')" ]; then
-        tmux a -t default
-else
-        tmux new -s default
-fi
+
