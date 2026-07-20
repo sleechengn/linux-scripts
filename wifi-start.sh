0d9fdb2b3018bb0fb56d44a382e5613ac606f184
lee <sleechengn@live.cn>
2026-07-20 14:18:03 +0800

    保存

--- wifi-start.sh
+++ wifi-start.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-ip link set wlp3s0 up
-#wpa_cli -i wlp3s0 disconnect
-#iw dev wlp3s0 disconnect
-SSID=$(iw wlp3s0 scan|grep SYJD|grep 8306|awk '{print $2}')
+IFN="wlp3s0"
+
+ip link set $IFN up
+#wpa_cli -i $IFN disconnect
+#iw dev $IFN disconnect
+SSID=$(iw $IFN scan|grep SYJD|grep 8306|awk '{print $2}')
 echo "SSID=$SSID"
 
 if [ "$SSID" ]; then
         wpa_passphrase $SSID 88888888 > /tmp/wpa_supplicant.conf
-        wpa_supplicant -B -i wlp3s0 -c /tmp/wpa_supplicant.conf
+        wpa_supplicant -B -i $IFN -c /tmp/wpa_supplicant.conf
 
         SETTING_LOOP=1
         while [ $SETTING_LOOP -eq 1 ]; do
-                if [ "$(iw dev wlp3s0 link|grep SSID)" ]; then
+                if [ "$(iw dev $IFN link|grep SSID)" ]; then
                         echo "connected"
-                        dhclient wlp3s0
+                        dhclient $IFN
                         SETTING_LOOP=0
                 else
                         sleep 1
