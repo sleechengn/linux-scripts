87e8e574d854e266b244a52c1a837afb543caba6
lee <sleechengn@live.cn>
2026-07-22 07:49:51 +0800

    保存

--- ethernet-boot.sh
+++ ethernet-boot.sh
@@ -3,6 +3,9 @@ IFNAME=enp4s0
 if [ "$1" ]; then
         IFNAME="$1"
 fi
+if [ ! "$(which ethtool)" ]; then
+        apt install -y ethtool
+fi
 if [ "$(ip a|grep $IFNAME)" ] && [ "$(which ethtool)" ]; then
         ip link set $IFNAME up
         if [ "$(ethtool $IFNAME|grep Link|grep detected|grep yes)" ]; then
