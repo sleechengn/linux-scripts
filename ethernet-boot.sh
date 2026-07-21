e7d18c7b684dda47878e32c6b23179334f4f3561
lee <sleechengn@live.cn>
2026-07-22 07:49:07 +0800

    保存

--- ethernet-boot.sh
+++ ethernet-boot.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-
-if [ "$(ip a|grep enp4s0)" ] && [ "$(which ethtool)" ]; then
-        ip link set enp4s0 up
-        if [ "$(ethtool enp4s0|grep Link|grep detected|grep yes)" ]; then
-                echo "enp4s0 activate"
-                dhclient enp4s0 > /dev/null 2>&1 &
+IFNAME=enp4s0
+if [ "$1" ]; then
+        IFNAME="$1"
+fi
+if [ "$(ip a|grep $IFNAME)" ] && [ "$(which ethtool)" ]; then
+        ip link set $IFNAME up
+        if [ "$(ethtool $IFNAME|grep Link|grep detected|grep yes)" ]; then
+                echo "$IFNAME activate"
+                dhclient $IFNAME > /dev/null 2>&1 &
         fi
 fi
