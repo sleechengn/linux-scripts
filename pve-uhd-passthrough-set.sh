dc444dd7fbcf8eccd84c88057240212f56224c96
lee <sleechengn@live.cn>
2026-07-24 21:28:13 +0800

    保存

--- pve-uhd-passthrough-set.sh
+++ pve-uhd-passthrough-set.sh
@@ -22,7 +22,7 @@ if [ "$1" ]; then
         qm set $VMID -vga none
         qm set $VMID -hookscript pvevm-hooks-uhd:snippets/pvevm-hooks-uhd.sh
         qm set $VMID -tablet 0
-        echo "请注意：已经设置了大部分参数，但是romfile，evdev需要修改"
+        echo "请注意：已经设置了大部分参数，但是romfile，evdev需要修改，配合pvevm-hooks-uhd使用"
 else
         echo "usage ./macos-inject.sh vmid"
 fi
