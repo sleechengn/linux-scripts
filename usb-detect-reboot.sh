5aae12c3e28d6aee6c9a63de9c156a1136f7cc48
lee <sleechengn@live.cn>
2026-07-26 19:56:12 +0800

    保存

--- usb-detect-reboot.sh
+++ usb-detect-reboot.sh
@@ -1,9 +1,46 @@
 #!/usr/bin/env bash
+set -x
 USBID=$1
 
 if [ ! "$USBID" ]; then
-    echo "please input usb id eg. 248d:5b5e"
-else
+
+    if ! command -v dialog &> /dev/null; then
+        echo "not found dialog (eg. sudo apt install dialog)"
+        exit 1
+    fi
+    if ! command -v lsusb &> /dev/null; then
+        echo "not found usbutils (eg. sudo apt install usbutils)"
+        exit 1
+    fi
+
+    MENU_OPTIONS=()
+    while IFS= read -r line; do
+        echo $line
+        usb_id=$(echo "$line" | grep -oP 'ID [0-9a-fA-F]{4}:[0-9a-fA-F]{4}' | awk '{print $2}')
+        echo $usb_id
+        if [ ! -z "$usb_id" ]; then
+            MENU_OPTIONS+=("$usb_id" "$line")
+        fi
+    done < <(lsusb)
+
+    if [ ${#MENU_OPTIONS[@]} -eq 0 ]; then
+        dialog --title "alter" --msgbox "not found USB device" 6 40
+        exit 1
+    fi
+
+    TMP_FILE=$(mktemp)
+    dialog --clear \
+       --title "USB Selector" \
+       --menu "Please select USB device (Use up down Enter):" \
+       20 80 10 \
+       "${MENU_OPTIONS[@]}" 2> "$TMP_FILE"
+    if [ $? -eq 0 ]; then
+        USBID=$(cat $TMP_FILE|head -n 1)
+        echo select $USBID
+    else
+        exit 1
+    fi
+fi
 
 if [ "$USBID" == "-r" ]; then
     systemctl stop udr
@@ -50,5 +87,4 @@ EOF
 systemctl enable --now udr
 fi
 
-fi
 fi
