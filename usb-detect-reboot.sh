bbe467f826121f437aa60c828bef282a769cbc59
lee <sleechengn@live.cn>
2026-07-26 09:43:29 +0800

    save

--- usb-detect-reboot.sh
+++ usb-detect-reboot.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+USBID=$1
+
+if [ ! "$USBID" ]; then
+    echo "please input usb id eg. 248d:5b5e"
+else
+
+if [ "$USBID" == "-r" ]; then
+    systemctl stop udr
+    systemctl disable udr
+    ps -ef|grep udr.sh|awk '{print $2}'|xargs -i kill -9 {}
+else
 
 mkdir -p /opt/udr
 if [ ! -e "/opt/udr/udr.sh" ]; then
@@ -6,7 +17,7 @@ cat > /opt/udr/udr.sh <<EOF
 #!/usr/bin/env bash
 KEEP="true"
 while [ \$KEEP == "true" ]; do
-    if [ "\$(lsusb|grep -F 248d|grep -F 5b5e)" ]; then
+    if [ "\$(lsusb|grep -F '$USBID')" ]; then
         echo "usb detected, keep"
     else
         qm list |grep running|awk '{print \$1}'|grep -v 101|grep -v 103|xargs -i qm stop {}
@@ -34,4 +45,7 @@ Type=simple
 WantedBy=multi-user.target
 EOF
 systemctl enable --now udr
+fi
+
+fi
 fi
