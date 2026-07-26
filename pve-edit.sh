8c1743f7c64d73960ec5e9b778ea66a7ff56ead4
lee <sleechengn@live.cn>
2026-07-26 20:36:37 +0800

    sa

---
+++ pve-edit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$(ls -A /etc/pve/qemu-server)" ]; then
+    MENU_OPTIONS=()
+    for file in /etc/pve/qemu-server/*; 
+    do
+        echo $file
+        MENU_OPTIONS+=("$file" "$(cat $file|grep name|awk '{print $2}')")
+    done
+    if [ ${#MENU_OPTIONS[@]} -eq 0 ]; then
+        dialog --msgbox "未找到有效数据！" 8 40
+        exit 1
+    fi
+    CHOICE=$(dialog --clear \
+                --title "Virtual Machine List" \
+                --menu "Please select which" 18 60 8 \
+                "${MENU_OPTIONS[@]}" 2>&1 >/dev/tty)
+    if [ $? -eq 0 ]; then
+        nano $CHOICE
+    fi
+fi
