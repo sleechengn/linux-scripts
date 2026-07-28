019960b1142cfb2742db155a6086686994bf194f
lee <sleechengn@live.cn>
2026-07-28 08:04:44 +0800

    sav

--- show-image.sh
+++ show-image.sh
@@ -12,7 +12,7 @@ fi
 if [ -e "/root/.bg.jpg" ]; then
     if [ "$(command -v fbi)" ]; then
         fbi /root/.bg.jpg
-        if [ ! $? -eq 0]; then
+        if [ ! $? -eq 0 ]; then
             if [ "$(command -v chafa)" ]; then
                 chafa /root/.bg.jpg
             fi
