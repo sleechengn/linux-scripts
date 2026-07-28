b2cad5c767919ac1aa3ddc81cad59122b5e5d503
lee <sleechengn@live.cn>
2026-07-28 08:04:06 +0800

    sav

--- show-image.sh
+++ show-image.sh
@@ -4,8 +4,22 @@ if [ "$(command -v apt)" ]; then
     if [ ! "$(command -v fbi)" ]; then
         apt install -y fbi
     fi
+    if [ ! "$(command -v chafa)" ]; then
+        apt install -y chafa
+    fi
 fi
 
 if [ -e "/root/.bg.jpg" ]; then
-    fbi /root/.bg.jpg
+    if [ "$(command -v fbi)" ]; then
+        fbi /root/.bg.jpg
+        if [ ! $? -eq 0]; then
+            if [ "$(command -v chafa)" ]; then
+                chafa /root/.bg.jpg
+            fi
+        fi
+    else
+        if [ "$(command -v chafa)" ]; then
+            chafa /root/.bg.jpg
+        fi
+    fi
 fi
