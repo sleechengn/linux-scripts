96e4fc409f810e83919d44215394a0979d631d12
lee <sleechengn@live.cn>
2026-08-02 13:11:05 +0800

    sav

--- debian-shell-auto-login.sh
+++ debian-shell-auto-login.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-find /usr/lib/|grep -F /getty@|xargs -i sed -i 's,^ExecStart.*,ExecStart=-/sbin/agetty --noclear %I $TERM --autologin root,g' {}
+if [ "$1" ]; then
+    find /usr/lib/|grep -F /getty@|xargs -i sed -i "s,^ExecStart.*,ExecStart=-/sbin/agetty --noclear %I $TERM --autologin $1,g" {}
+else
+    find /usr/lib/|grep -F /getty@|xargs -i sed -i 's,^ExecStart.*,ExecStart=-/sbin/agetty --noclear %I $TERM --autologin root,g' {}
+fi
 systemctl daemon-reload
 systemctl disable getty@tty1.service
 systemctl enable getty@tty1.service
