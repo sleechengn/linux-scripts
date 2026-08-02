7986f40e37e653bd56bef3f8c5a2168815cb45cd
lee <sleechengn@live.cn>
2026-08-02 13:01:32 +0800

    保存程序

--- deb-import-ca.sh
+++ deb-import-ca.sh
@@ -2,8 +2,14 @@
 
 
 if [ "$1" ]; then
-    cp $1 /usr/local/share/ca-certificates/
-    update-ca-certificates
+    if [ $(id -u $(whoami)) -eq 0 ]; then
+        cp $1 /usr/local/share/ca-certificates/
+        update-ca-certificates
+    else
+        sudo cp $1 /usr/local/share/ca-certificates/
+        sudo update-ca-certificates
+    fi
+    
 else
     echo "usage: ./deb-import-ca.sh [path/to/crt]"
 fi
