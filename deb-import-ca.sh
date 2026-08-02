ae9daa2690b66f3a8e47de3edc9137cecad342cf
lee <sleechengn@live.cn>
2026-08-02 12:58:51 +0800

    保存程序

--- deb-import-ca.sh
+++ deb-import-ca.sh
@@ -5,5 +5,5 @@ if [ "$1" ]; then
     cp $1 /usr/local/share/ca-certificates/
     update-ca-certificates
 else
-    echo "usage: ./deb-import-ca.sh [path/to/crt]
+    echo "usage: ./deb-import-ca.sh [path/to/crt]"
 fi
