301e561597940b5279eb361daf93557dc61cc211
lee <sleechengn@live.cn>
2026-09-24 13:03:40 +0800

    sav

--- pve-menu.sh
+++ pve-menu.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -x
+
+if [ ! $EUID -eq 0 ]; then
+    dialog --msgbox "please exec root" 8 40
+    exit 1
+fi
 
 # 1. 加载虚拟机列表
 MENU_OPTIONS=()
@@ -8,7 +14,7 @@ done < <( qm list|sed 1d|awk '{print $1,$2,$3}')
 
 # 2. 检查数组是否为空
 if [ ${#MENU_OPTIONS[@]} -eq 0 ]; then
-    dialog --msgbox "notfounddat" 8 40
+    dialog --msgbox "not found vm" 8 40
     exit 1
 fi
 
